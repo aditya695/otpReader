77303d10f304cee0d1da805d4ef583bc4e784910
aditya695 <aditya_wadekar@ymail.com>
2019-11-06 15:25:03 +0530

    added read OTP change

--- app/src/main/java/com/example/otpreader/OtpReader.kt
+++ app/src/main/java/com/example/otpreader/OtpReader.kt
@@ -3,6 +3,10 @@ package com.example.otpreader
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import java.util.regex.Pattern
 
 
 class OtpReader () : AppCompatActivity()
@@ -11,5 +15,20 @@ class OtpReader () : AppCompatActivity()
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+    }
+
+    fun readOTP(msg: String): String {
+        for (otpCollection in MyOtpCollection.values()) {
+            val pattern = Pattern.compile(otpCollection.getOtpType(), Pattern.MULTILINE)
+            val matcher = pattern.matcher(msg)
+            while (matcher.find()) {
+                //System.out.println("Full match: " + matcher.group(0));
+                for (i in 1..1) {
+                    // System.out.println("Group " + i + ": " + matcher.group(i));
+                    return matcher.group(i)
+                }
+            }
+        }
+        return msg
     }
 }
